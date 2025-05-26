07a52b9a59327cf3318636a030efc033aaf42d30
voidtools <31149593+voidtools@users.noreply.github.com>
2025-05-26 15:41:22 +0930

    Add files via upload

---
+++ setup/res/build32.bat
@@ -0,0 +1,2 @@
+del "C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2"
+"C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2 "C:\dev\everything\plugins\http_server\Release\http_server32.dll" -mx=9
---
+++ setup/res/build64.bat
@@ -0,0 +1,2 @@
+del "C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2"
+"C:\Program Files\7-Zip\7z.exe" a -tbzip2 C:\dev\everything\plugins\http_server\setup\res\setup.dll.bz2 "C:\dev\everything\plugins\http_server\x64\Release\http_server64.dll" -mx=9
