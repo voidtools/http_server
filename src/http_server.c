85d467edbd3adf1b6fa16932b05e1265e795159a
voidtools <31149593+voidtools@users.noreply.github.com>
2025-05-26 15:35:13 +0930

    Add files via upload

--- src/http_server.c
+++ src/http_server.c
@@ -1,5 +1,26 @@
+//
+// Copyright 2025 voidtools / David Carpenter
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 
-//TODO:
+// TODO:
 // add support for the date created column.
 // a simple path rewrite to hide roots, for example: c:\private\media => /media
 // add support for date created column (mostly for json support)
@@ -62,7 +83,7 @@ enum
 
 #include <windows.h>
 #include "..\..\include\everything_plugin.h"
-#include "../version.h"
+#include "version.h"
 
 #define HTTP_SERVER_STRING_MACRO(name,lowercase_ascii_name,string) HTTP_SERVER_STRING_##name,
 enum
